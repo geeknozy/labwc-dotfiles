807b6b4342f09550e331c8a57174a3ee3ca0a31e
geeknozy <71586323+geeknozy@users.noreply.github.com>
2026-01-16 12:55:23 +0530

    Update brightness_notify.sh

--- bin/scripts/brightness_notify.sh
+++ bin/scripts/brightness_notify.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
+# This updates and grabs the new value in a single command execution
+NEW_VAL=$(brightnessctl set ${1:-up} 10% -q -m | cut -d, -f4)
 
-case "$1" in
-  up)   brightnessctl set +10% ;;
-  down) brightnessctl set 10%- ;;
-esac
-
-BR=$(brightnessctl get)
-MAX=$(brightnessctl max)
-PCT=$(( BR * 100 / MAX ))
-
-notify-send "Brightness" "$PCT%"
+notify-send -t 800 -h string:x-canonical-private-synchronous:brightness "Brightness" "$NEW_VAL"
